24574962df01bbe7d8cba9ee7c7cecf163e1367d
Japhet Seumo <seumojaphet555@gmai.com>
2024-11-14 17:02:38 +0100

    fecth api movie

--- src/App.js
+++ src/App.js
@@ -4,11 +4,35 @@ import Home from "./components/Home";
 import AboutView from "./components/AboutView";
 import SearchView from "./components/SearchView";
 import { Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [searchResults, setSearchResults] = useState([]);
   const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    console.log(searchText + " is the searched text");
+
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNWU5YzYxNDBlZDYwMzJiNzBiY2I4ZWU3YmE4ZGE0ZCIsIm5iZiI6MTczMTUxODk2Ni44NTU3NTQsInN1YiI6IjY3MzRiODBhNzZhZjNhZTdiNjM4YzllYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XeIj-ms_90sC3ZCoj83iHyzVtVUbRJsrnY17iV7hDZs",
+      },
+    };
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${searchText}&include_adult=false&language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setSearchResults(data.results);
+        // console.log(setSearchResults(data.results));
+      })
+      .catch((err) => console.error(err));
+  }, [searchText]);
 
   return (
     <div>
--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -1,9 +1,15 @@
 import Hero from "./Hero";
 
 // TMDB API KEY = e5e9c6140ed6032b70bcb8ee7ba8da4d
+/* Example link for Movie seraches = curl --request GET \
+     --url 'https://api.themoviedb.org/3/search/movie?query=star%20wars&include_adult=false&language=en-US&page=1' \
+     --header 'Authorization: Bearer e5e9c6140ed6032b70bcb8ee7ba8da4d' \
+     --header 'accept: application/json'
+*/
 
 const SearchView = ({ keyword, searchResults }) => {
   const title = `You are searching for ${keyword}`;
+  console.log(searchResults + " are the results");
   return (
     <>
       <Hero text={title} />
